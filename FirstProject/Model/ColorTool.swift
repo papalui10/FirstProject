dfd664e93f2c45c97334521c71f3e3a482bcbb7f
Luis Veliz <luisvelizjr@gmail.com>
2018-10-09 12:20:43 -0600

    Added Model Group and renamed files.

---
+++ FirstProject/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstProject
+//
+//  Created by Veliz, Luis on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
